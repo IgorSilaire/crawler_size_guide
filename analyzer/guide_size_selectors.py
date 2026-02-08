c2a91647927870e5c461af23762ad71ce445ec6e
IgorSilaire <igor.silaire@gmail.com>
2026-02-08 18:00:09 +0100

    fix crawler/browser.py and key words for guide size in analyzer/guide_size_selectors.py

---
+++ analyzer/guide_size_selectors.py
@@ -0,0 +1,12 @@
+# analyzer/guide_size_selectors.py
+
+SIZE_GUIDE_SELECTORS = [
+    "guide des tailles",
+    "guide de taille",
+    "guide tailles",
+    "size guide",
+    "size chart",
+    "tailles",
+    "tableau des tailles",
+    "pointure"
+]
