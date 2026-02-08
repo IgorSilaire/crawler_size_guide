d217bace65b541bc05bfead105b0f989a0ea1559
IgorSilaire <igor.silaire@gmail.com>
2026-02-08 11:54:18 +0100

    file analyzer/cookies.py created (empty) + finished crawler/browser.py and crawler/crawwler.py

---
+++ crawler/browser.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import sync_playwright
+
+class Browser:
+    def __init__(self, headless: bool = True):
+        self.headless = headless
+        def __enter__(self):
+            self.playwright = sync_playwright().start()
+            self.browser = self.playwright.chromium.launch(headless=self.headless)
+            
+            self.context = self.browser.new_context(
+                viewport={"width": 1366, "height": 900},
+                ignore_https_errors=True,
+                user_agent=(
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/122.0.0.0 Safari/537.36"
+                ),
+            )
+            self.page = self.context.new_page()
+            return self.page
+        
+        def __exit__(self, exc_type, exc, tb):
+            self.context.close()
+            self.browser.close()
+            self.playwright.stop()
+        
+        
+                
+
+
+
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,24 +1,9 @@
-from playwright.sync_api import sync_playwright
+from crawler.browser import Browser
+from analyzer.cookies import accept_cookies
 
-class Browser:
-    def __init__(self, headless: bool = True):
-        self.headless = headless
-        def __enter__(self):
-            self.playwright = sync_playwright().start()
-            self.browser = self.playwright.chromium.launch(headless=self.headless)
-            
-            self.context = self.browser.new_context(
-                viewport={"width": 1366, "height": 900},
-                ignore_https_errors=True,
-                user_agent=(
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                    "AppleWebKit/537.36 (KHTML, like Gecko) "
-                    "Chrome/122.0.0.0 Safari/537.36"
-                ),
-            )
-            self.page = self.context.new_page()
-            return self.page
-                
-
-
-
+def crawl(url :str, headless: bool = True) -> str:
+    with Browser(headless=headless) as page:
+        page.goto(url, timeout = 60_000)
+        accept_cookies(page)
+        page.wait_for_load_state("networkidle")
+        return page.content() 
